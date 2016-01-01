47b702afc83f91c433fe35e48a48f7aee45ef3b5
Dmitrij D. Czarkoff <czarkoff@gmail.com>
2016-01-01 12:28:14 +0100

    Include <limits.h>

--- src/main.c
+++ src/main.c
@@ -15,6 +15,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+#include <limits.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <stdint.h>
